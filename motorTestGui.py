3c3be1804126e847206c3e949c01232dd9fe57a6
root <root@raspberrypi>
2021-03-07 17:46:51 -0800

    Added setting for serial port. Cycle number now an Entry box... allows for easier entry. :)

--- motorTestGui.py
+++ motorTestGui.py
@@ -11,11 +11,12 @@ def readAllLines():
           break
       print(buff)
 
+ser = ""
 root = tk.Tk()
 root.title("As the motor turns")
 speedVar = tk.IntVar()
 distanceVar = tk.IntVar()
-cyclesVar = tk.IntVar()
+cyclesVar = tk.StringVar()
 OptionList = [
 "Clockwise",
 "Counter-Clockwise"
@@ -26,7 +27,8 @@ directionVar.set(OptionList[0])
 def runCycles():
   speed = speedVar.get()
   distance = distanceVar.get()
-  cycles = cyclesVar.get()
+  cycles = int(cyclesVar.get())
+  print(cycles)
   fwdCommand = "G1 F" + str(speed) + " X" + str(distance) + " H0 \r\n"
   revCommand = "G1 F" + str(speed) + " X-" + str(distance) + " H0 \r\n"
   print(fwdCommand)
@@ -59,7 +61,23 @@ def runPositionCCW():
   ser.write(revCommand.encode())
   print(revCommand)
   print(ser.readline())
-  
+
+## Main Section
+def initSystem():
+    global ser
+    print(serialPort.get())
+    ser = serial.Serial(serialPort.get())
+    ser.baudrate = 250000
+    ser.timeout = 1
+    readAllLines()
+    ser.write("M119\r\n".encode())
+    readAllLines()
+    ser.write("M92 X16.00\r\n".encode()) # Try and set 1 mm = 1 step
+    readAllLines()
+    ser.write("M203 X800\r\n".encode()) # Set Max Speeds
+    readAllLines()
+    ser.write("M201 X4000\r\n".encode()) # Set Max Acceleration
+    readAllLines()
 
 tabControl = ttk.Notebook(root)
 cycleTab = ttk.Frame(tabControl)
@@ -67,24 +85,28 @@ posTab = ttk.Frame(tabControl)
 
 ## Cycle Tab
 tabControl.add(cycleTab, text = "Cycle")
+#lazily adding the serial port entry here....
+serialPort = tk.StringVar(value = "/dev/ttyUSB0")
+spBox = tk.Entry(cycleTab, textvariable = serialPort, ).grid(row=0, column=1, columnspan=1)
+initButton = tk.Button(cycleTab, text="Init System", command = initSystem).grid(row=0, column = 2)
+
 var = tk.StringVar()
 label = tk.Label( cycleTab, textvariable=var).grid(row=5, column=2, columnspan=1)
 
-speedSlider = tk.Scale(cycleTab, from_=0, to_=40000, variable = speedVar,
+speedSlider = tk.Scale(cycleTab, from_=0, to_=50000, variable = speedVar,
                                  orient=tk.HORIZONTAL, resolution = 100, label = "Speed",
-                                 width=45, length=400).grid(row=0, column=0, columnspan=2)
+                                 width=45, length=400).grid(row=2, column=0, columnspan=2)
 
 distanceSlider = tk.Scale(cycleTab, from_=0, to_=500, variable = distanceVar,
                                  orient=tk.HORIZONTAL, resolution = 1, label = "Distance",
-                                 width=45, length=400).grid(row=1, column=0, columnspan=2)
-
-cyclesSlider = tk.Scale(cycleTab, from_=1, to_=1000, variable = cyclesVar,
-                                 orient=tk.HORIZONTAL, resolution = 1, label = "Cycles",
                                  width=45, length=400).grid(row=3, column=0, columnspan=2)
 
-goButton = tk.Button(cycleTab, text = "CYCLE", command = runCycles).grid(row = 4, column=1)
-goRoundButton = tk.Button(cycleTab, text = "GO Right", command = runPositionCW).grid(row = 5, column=2)
-goBackButton = tk.Button(cycleTab, text = "GO Left", command = runPositionCCW).grid(row = 5, column=3)
+tk.Label(cycleTab, text="Number of cycles").grid(row=4, column = 0)
+cycles = tk.Entry(cycleTab, textvariable = cyclesVar).grid(row=4, column=1, columnspan=1)
+
+goButton = tk.Button(cycleTab, text = "CYCLE", command = runCycles).grid(row = 5, column=1)
+goRoundButton = tk.Button(cycleTab, text = "GO Right", command = runPositionCW).grid(row = 6, column=2)
+goBackButton = tk.Button(cycleTab, text = "GO Left", command = runPositionCCW).grid(row = 6, column=3)
 
 ## Position Tab
 
@@ -94,13 +116,6 @@ tabControl.add(posTab, text = "Position")
 
 tabControl.pack(expand=1, fill="both")
 
-## Main Section
-ser = serial.Serial("/dev/ttyUSB0")
-ser.baudrate = 250000
-ser.timeout = 1
-readAllLines()
-ser.write("M119\r\n".encode())
-readAllLines()
-ser.write("M92 X16.00\r\n".encode()) # Try and set 1 mm = 1 step
-readAllLines()
+
+
 root.mainloop()
